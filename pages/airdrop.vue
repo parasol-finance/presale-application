b4951543c1005b0cddfe36fa1aa619330cc5fc12
Clint.Network <me@clint.network>
2021-11-29 16:52:33 +0200

    Third try for telegram-login

--- pages/airdrop.vue
+++ pages/airdrop.vue
@@ -22,22 +22,23 @@
 				of both projects and investors alike.
 			</p>
 			<p class="text-lg font-semibold mb-3">Steps to follow to participate to the airdrop:</p>
-			<ol class="list-decimal pl-4 mb-5">
+			<ol class="list-decimal pl-4 mb-6">
 				<li>Login to Telegram using the button below 👇</li>
 				<li>Follow our channel <a class="text-pink-500" href="https://t.me/parasolfinance">@parasolfinance</a>.</li>
 			</ol>
 			<div class="flex flex-col items-center">
-				<vue-telegram-login
-					mode="callback"
-					telegram-login="ParasolFinanceBot"
-					:userpic="false"
-					@callback="handleTelegramConnection"/>
-				<button v-if="isTelegramConnected" @click="confirm" class="flex items-center justify-center text-xl mt-5 px-6 bg-pink-600 py-2 text-sm font-medium rounded-full opacity-100 shadow-lg text-gray-200 bg-gradient-to-r from-purple-500 to-pink-600 hover:from-pink-600 hover:to-purple-500">
+				<button v-if="isTelegramConnected" @click="confirm" class="flex items-center justify-center text-xl px-6 bg-pink-600 py-2 text-sm font-medium rounded-full opacity-100 shadow-lg text-gray-200 bg-gradient-to-r from-purple-500 to-pink-600 hover:from-pink-600 hover:to-purple-500">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
 					</svg>
 					Confirm Participation
 				</button>
+				<vue-telegram-login
+					v-else
+					mode="callback"
+					telegram-login="ParasolFinanceBot"
+					:userpic="false"
+					@callback="handleTelegramConnection"/>
 			</div>
 		</article>
 	</section>
